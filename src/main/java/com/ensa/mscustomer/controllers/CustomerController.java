8281cf83ff878e632971d2a127e2d3378146bbe2
Zahra <hamdane.zahra@gmail.com>
2023-01-05 08:02:28 +0100

    crud

--- src/main/java/com/ensa/mscustomer/controllers/CustomerController.java
+++ src/main/java/com/ensa/mscustomer/controllers/CustomerController.java
@@ -24,4 +24,24 @@ public class CustomerController {
     public CustomerResponseDto save(@RequestBody CustomerRequestDto customerRequestDto){
         return customerService.save(customerRequestDto);
     }
+
+    @GetMapping("id/{id}")
+    public CustomerResponseDto findById(@PathVariable Long id) {
+        return customerService.findById(id);
+    }
+
+    @GetMapping("name/{firstName}")
+    public CustomerResponseDto findByFirstName(@PathVariable String firstName) {
+        return customerService.findByFirstName(firstName);
+    }
+
+    @DeleteMapping("id/{id}")
+    public void delete(@PathVariable Long id) {
+        customerService.delete(id);
+    }
+
+    @PutMapping("id/{id}")
+    public CustomerResponseDto update(@RequestBody() CustomerRequestDto customerRequestDto, @PathVariable Long id) {
+        return customerService.update(customerRequestDto, id);
+    }
 }
